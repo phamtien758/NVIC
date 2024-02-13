4db0f99d384bd2979bc1bc7b382ee5f7b2a00bf3
phamtien758 <phamtien758@gmail.com>
2024-02-13 21:13:26 +0700

    Update template

--- driver/interrupt.c
+++ driver/interrupt.c
@@ -24,11 +24,11 @@
   */
 void Nvic_EnableIrq(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_ISER;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_ISER;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_ISER;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_ISER;
 
     NVIC->ISER[RegIndex_u32] |= NVIC_ISER_IRQ_MASK(BitIndex_u32);
 }
@@ -41,11 +41,11 @@ void Nvic_EnableIrq(Irq_Number IrqNum_e)
   */
 void Nvic_DisableIrq(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_ICER;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_ICER;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_ICER;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_ICER;
 
     NVIC->ICER[RegIndex_u32] |= NVIC_ICER_IRQ_MASK(BitIndex_u32);
 }
@@ -58,11 +58,11 @@ void Nvic_DisableIrq(Irq_Number IrqNum_e)
   */
 void Nvic_SetPendingIrq(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_ISPR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_ISPR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_ISPR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_ISPR;
 
     NVIC->ISPR[RegIndex_u32] |= NVIC_ISPR_IRQ_MASK(BitIndex_u32);
 }
@@ -75,11 +75,11 @@ void Nvic_SetPendingIrq(Irq_Number IrqNum_e)
   */
 void Nvic_ClearPendingIrq(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_ICPR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_ICPR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_ICPR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_ICPR;
 
     NVIC->ICPR[RegIndex_u32] |= NVIC_ICPR_IRQ_MASK(BitIndex_u32);
 }
@@ -91,22 +91,22 @@ void Nvic_ClearPendingIrq(Irq_Number IrqNum_e)
   * @retval  TRUE - IRQ is pending state
   *          FALSE - IRQ isn't pending state
   */
-uint8_t Nvic_IsPendingIrq(Irq_Number IrqNum_e)
+uint8 Nvic_IsPendingIrq(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
-    uint8_t RetValue_u8;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
+    uint8 RetValue_u8;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_ICPR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_ICPR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_ICPR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_ICPR;
 
     if(0U != (NVIC->ICPR[RegIndex_u32] & NVIC_ICPR_IRQ_MASK(BitIndex_u32)))
     {
-        RetValue_u8 = (uint8_t)TRUE;
+        RetValue_u8 = (uint8)TRUE;
     }
     else
     {
-        RetValue_u8 = (uint8_t)FALSE;
+        RetValue_u8 = (uint8)FALSE;
     }
 
     return RetValue_u8;
@@ -121,11 +121,11 @@ uint8_t Nvic_IsPendingIrq(Irq_Number IrqNum_e)
   */
 void Nvic_SetPriority(Irq_Number IrqNum_e, Irq_Pri PriNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_IPR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_IPR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_IPR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_IPR;
     
     NVIC->IPR[RegIndex_u32] |= (PriNum_e << NVIC_IPR_IRQ_POS(BitIndex_u32));
 }
@@ -134,18 +134,18 @@ void Nvic_SetPriority(Irq_Number IrqNum_e, Irq_Pri PriNum_e)
   * @brief   Return priority value of an interrupt
   * @note    None
   * @param   IrqNum_e   IRQ number.
-  * @retval  uint8_t - Priority value
+  * @retval  uint8 - Priority value
   */
-uint8_t Nvic_GetPriority(Irq_Number IrqNum_e)
+uint8 Nvic_GetPriority(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
-    uint8_t RetValue_u8;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
+    uint8 RetValue_u8;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_IPR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_IPR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_IPR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_IPR;
     
-    RetValue_u8 = (uint8_t)((NVIC->IPR[RegIndex_u32] & \
+    RetValue_u8 = (uint8)((NVIC->IPR[RegIndex_u32] & \
                             NVIC_IPR_IRQ_MASK(BitIndex_u32)) >> \
                             NVIC_IPR_IRQ_POS(BitIndex_u32));
 
@@ -156,24 +156,24 @@ uint8_t Nvic_GetPriority(Irq_Number IrqNum_e)
   * @brief   Check if IRQ is active or not
   * @note    None
   * @param   IrqNum_e   IRQ number.
-  * @retval  uint8_t - Priority value
+  * @retval  uint8 - Priority value
   */
-uint8_t Nvic_IsActive(Irq_Number IrqNum_e)
+uint8 Nvic_IsActive(Irq_Number IrqNum_e)
 {
-    uint32_t RegIndex_u32;
-    uint32_t BitIndex_u32;
-    uint8_t RetValue_u8;
+    uint32 RegIndex_u32;
+    uint32 BitIndex_u32;
+    uint8 RetValue_u8;
 
-    RegIndex_u32 = (uint32_t)IrqNum_e / (uint32_t)NUM_IRQ_PER_NVIC_IABR;
-    BitIndex_u32 = (uint32_t)IrqNum_e % (uint32_t)NUM_IRQ_PER_NVIC_IABR;
+    RegIndex_u32 = (uint32)IrqNum_e / (uint32)NUM_IRQ_PER_NVIC_IABR;
+    BitIndex_u32 = (uint32)IrqNum_e % (uint32)NUM_IRQ_PER_NVIC_IABR;
 
     if(0U != (NVIC->ICPR[RegIndex_u32] & NVIC_IABR_IRQ_MASK(BitIndex_u32)))
     {
-        RetValue_u8 = (uint8_t)TRUE;
+        RetValue_u8 = (uint8)TRUE;
     }
     else
     {
-        RetValue_u8 = (uint8_t)FALSE;
+        RetValue_u8 = (uint8)FALSE;
     }
 
     return RetValue_u8;
--- driver/interrupt.h
+++ driver/interrupt.h
@@ -14,22 +14,22 @@
 /*** DEFINE *************************************/
 
 /* Number of IRQ in these register (register bit width / IRQ bit width) */
-#define NUM_IRQ_PER_NVIC_ISER    ((uint32_t)(NVIC_ISER_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_ISER    ((uint32)(NVIC_ISER_REG_WIDTH / \
                                              NVIC_ISER_BIT_PER_IRQ))
 
-#define NUM_IRQ_PER_NVIC_ICER    ((uint32_t)(NVIC_ICER_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_ICER    ((uint32)(NVIC_ICER_REG_WIDTH / \
                                              NVIC_ICER_BIT_PER_IRQ))
 
-#define NUM_IRQ_PER_NVIC_ISPR    ((uint32_t)(NVIC_ISPR_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_ISPR    ((uint32)(NVIC_ISPR_REG_WIDTH / \
                                              NVIC_ISPR_BIT_PER_IRQ))
 
-#define NUM_IRQ_PER_NVIC_ICPR    ((uint32_t)(NVIC_ICPR_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_ICPR    ((uint32)(NVIC_ICPR_REG_WIDTH / \
                                              NVIC_ICPR_BIT_PER_IRQ))
 
-#define NUM_IRQ_PER_NVIC_IABR    ((uint32_t)(NVIC_IABR_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_IABR    ((uint32)(NVIC_IABR_REG_WIDTH / \
                                              NVIC_IABR_BIT_PER_IRQ))
 
-#define NUM_IRQ_PER_NVIC_IPR    ((uint32_t)(NVIC_IPR_REG_WIDTH / \
+#define NUM_IRQ_PER_NVIC_IPR    ((uint32)(NVIC_IPR_REG_WIDTH / \
                                              NVIC_IPR_BIT_PER_IRQ))
 
 /*** TYPEDEF ************************************/
@@ -49,15 +49,15 @@ void Nvic_SetPendingIrq(Irq_Number IrqNum_e);
 void Nvic_ClearPendingIrq(Irq_Number IrqNum_e);
 
 /* Check if an interrupt is pending or not */
-uint8_t Nvic_IsPendingIrq(Irq_Number IrqNum_e);
+uint8 Nvic_IsPendingIrq(Irq_Number IrqNum_e);
 
 /* Set priority of interrupt */
 void Nvic_SetPriority(Irq_Number IrqNum_e, Irq_Pri PriNum_e);
 
 /* Get priority value of interrupt */
-uint8_t Nvic_GetPriority(Irq_Number IrqNum_e);
+uint8 Nvic_GetPriority(Irq_Number IrqNum_e);
 
 /* Check if an interrupt is pending or not */
-uint8_t Nvic_IsActive(Irq_Number IrqNum_e);
+uint8 Nvic_IsActive(Irq_Number IrqNum_e);
 
 #endif /* INTERRUPT_H */
